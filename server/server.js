16594d5beb187417e4839043c13c12a6b7c418eb
daveS <davesimpcontrol@gmail.com>
2023-05-20 12:24:44 -0600

    added comments of what needs to be done

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// add middleware and database connection
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// add connection to apollo server
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +18,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// add app get
+
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // i want to see the apollo server running
 });
+
+// we will have to add the apollo server here that we will define after the local port is defined
