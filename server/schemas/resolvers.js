7cfbad681382b55cb45221b75387f7cde54a8ee4
daveS <davesimpcontrol@gmail.com>
2023-05-20 12:37:36 -0600

    added schema folder with intended files, they are currently unchanged after porting over from mini project

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const { Book, User } = require('../models');
+
+//THIS MAY BE COMPLETELY WRONG
+
+const resolvers = {
+  Query: {
+    user: async () => {
+      return User.find({});
+    },
+    users: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return user.find(params);
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await user.create(args);
+      return user;
+    },
+    createVote: async (parent, { _id, userNum }) => {
+      const vote = await User.findOneAndUpdate(
+        { _id },
+        { $inc: { [`user${userNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
