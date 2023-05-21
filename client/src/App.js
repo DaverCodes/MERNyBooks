31b066f416781bfe5dabd4ec1a26bd7c2b372d20
daveS <davesimpcontrol@gmail.com>
2023-05-21 07:53:15 -0600

    added imports, apollo client and graohql

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,39 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 // import Apollo stuff
-//get graphql stuff
+import { 
+  ApolloClient, 
+  ApolloProvider, 
+  InMemoryCache, 
+  createHttpLink } 
+    from '@apollo/client';
 
-//get and authorize token info
+  import { setContext } 
+    from '@apollo/client/link/context';
+
+
+//get graphql stuff.. how do i authorize token info?
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
 
 // show user info to user
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 function App() {
   return (
